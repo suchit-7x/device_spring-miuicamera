35c7c500ea8a73380095c4c42c3f94f40b9b68e7
suchit-7x <gtsuchit7x@gmail.com>
2026-06-26 11:34:15 +0530

    spring-miuicamera: Rebrand for spring

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,9 +5,9 @@
 #
 
 # Inherit from the proprietary version
-include vendor/xiaomi/onyx-miuicamera/BoardConfigVendor.mk
+include vendor/xiaomi/spring-miuicamera/BoardConfigVendor.mk
 
-MIUICAMERA_PATH := device/xiaomi/onyx-miuicamera
+MIUICAMERA_PATH := device/xiaomi/spring-miuicamera
 
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
